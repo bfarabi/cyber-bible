2007f9f82cd19e4a24600a1a9b6ef20abb17770e
bfarabi <bfarab108@gmail.com>
2023-03-05 23:44:17 +0600

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}","./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
     extend: {},
   },
 
-  plugins: [require("daisyui")],
-
+  plugins: [require("daisyui","tw-elements/dist/plugin")],
+  
 }
